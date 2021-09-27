afa7bb47de4198f895ed1bf7ebdbb290a96d70a0
M. Zulqarnain <zulqarnain.mailbox@gmail.com>
2021-09-27 17:21:22 +0500

    fix: quality failures

--- lms/djangoapps/discussion/urls.py
+++ lms/djangoapps/discussion/urls.py
@@ -10,8 +10,7 @@ from . import views
 urlpatterns = [
     re_path(r'users/(?P<user_id>\w+)/followed$', views.followed_threads, name='followed_threads'),
     re_path(r'users/(?P<user_id>\w+)$', views.user_profile, name='user_profile'),
-    re_path(r'^(?P<discussion_id>[\w\-.]+)/threads/(?P<thread_id>\w+)$', views.single_thread,
-        name='single_thread'),
+    re_path(r'^(?P<discussion_id>[\w\-.]+)/threads/(?P<thread_id>\w+)$', views.single_thread, name='single_thread'),
     re_path(r'^(?P<discussion_id>[\w\-.]+)/inline$', views.inline_discussion, name='inline_discussion'),
     path(
         'discussion_board_fragment_view',
